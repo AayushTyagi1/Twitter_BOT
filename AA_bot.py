cfe39a5ade2bbfa45aa627c8c1d2ca772373adb9
Aayush Tyagi <tyagiaayush5@gmail.com>
2020-07-17 15:25:20 +0530

    Update AA_bot.py

--- AA_bot.py
+++ AA_bot.py
@@ -1,1 +1,20 @@
+import tweepy
+print("This is my Twitter bot")
+consumer_key = ""
+consumer_secret = ""
 
+Access_key = ""
+Access_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(Access_key, Access_secret)
+
+api=tweepy.API(auth)
+
+mentions=api.mentions_timeline()
+''''
+print(mentions[0].__dict__.keys())
+print(mentions[0].text)
+print(mentions[0].id) '''
+for mention in mentions:
+    print(str(mention.id) +" - " + mention.text)
